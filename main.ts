62a8c27e4192ac0b2818e6b45facedbccb04ba04
Firo <mail@firo.it>
2020-07-24 13:41:02 +0200

    forever loop and new icon

--- main.ts
+++ main.ts
@@ -1,25 +1,24 @@
-input.onButtonPressed(Button.A, function () {
-    if (currentValue > 900) {
-        basic.showIcon(IconNames.Surprised)
-    } else if (currentValue >= 600) {
-        basic.showIcon(IconNames.Heart)
-    } else {
-        basic.showIcon(IconNames.Sad)
-    }
-})
 input.onButtonPressed(Button.B, function () {
     led.plotBarGraph(
     resultValue,
     100
     )
 })
-let resultValue = 0
+let mapVar = 0
 let currentValue = 0
-currentValue = pins.analogReadPin(AnalogPin.P1)
+let resultValue = 0
 let minValue = 290
 let maxValue = 1020
-resultValue = (currentValue - minValue) / (maxValue - minValue) * 100
-let mapVar = Math.map(currentValue, minValue, maxValue, 0, 100)
 basic.forever(function () {
-	
+    currentValue = pins.analogReadPin(AnalogPin.P1)
+    resultValue = (currentValue - minValue) / (maxValue - minValue) * 100
+    mapVar = Math.map(currentValue, minValue, maxValue, 0, 100)
+    if (currentValue > 900) {
+        basic.showIcon(IconNames.Duck)
+    } else if (currentValue >= 600) {
+        basic.showIcon(IconNames.Heart)
+        basic.showIcon(IconNames.SmallHeart)
+    } else {
+        basic.showIcon(IconNames.Surprised)
+    }
 })
