089f5af02150f63f3fcf40a5a04209aba713427f
Amila Sampath <lucksy@gmail.com>
2020-10-20 21:49:37 +0200

    Added LEDs (Red,Blue)

--- main.ts
+++ main.ts
@@ -2,18 +2,26 @@ input.onButtonPressed(Button.A, function () {
     currentValue = pins.analogReadPin(AnalogPin.P1)
     resultValue = (currentValue - minValue) / (maxValue - minValue) * 100
     mapVar = Math.map(currentValue, minValue, maxValue, 0, 100)
-    if (currentValue > 900) {
+    if (currentValue > 725) {
         basic.showIcon(IconNames.Umbrella)
+        pins.digitalWritePin(DigitalPin.P2, 1)
+        pins.digitalWritePin(DigitalPin.P0, 1)
         basic.pause(2000)
-    } else if (currentValue >= 600) {
+    } else if (currentValue >= 400) {
         for (let index = 0; index < 2; index++) {
+            pins.digitalWritePin(DigitalPin.P2, 1)
+            pins.digitalWritePin(DigitalPin.P0, 0)
             basic.showIcon(IconNames.Heart)
             basic.showIcon(IconNames.SmallHeart)
         }
     } else {
         basic.showIcon(IconNames.Sad)
+        pins.digitalWritePin(DigitalPin.P2, 0)
+        pins.digitalWritePin(DigitalPin.P0, 1)
         basic.pause(2000)
     }
+    pins.digitalWritePin(DigitalPin.P2, 0)
+    pins.digitalWritePin(DigitalPin.P0, 0)
     basic.clearScreen()
 })
 input.onButtonPressed(Button.B, function () {
@@ -29,6 +37,8 @@ let maxValue = 0
 let minValue = 0
 minValue = 290
 maxValue = 1023
+pins.digitalWritePin(DigitalPin.P2, 0)
+pins.digitalWritePin(DigitalPin.P0, 0)
 basic.forever(function () {
 	
 })
